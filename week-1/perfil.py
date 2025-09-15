e67c385f4749d058e0be7a2e4c1e72a7735b18ea
William-AViana <willdev.viana@gmail.com>
2025-09-14 21:22:15 -0300

    file for demonstrate informations

---
+++ week-1/perfil.py
@@ -0,0 +1,4 @@
+informações = input('Digite seu nome, sua idade e a cidade onde você mora:\n').split(', ')
+
+print(f'Seu nome é {informações[0]}, sua idade é {informações[1]} e a cidade atual é '\
+      f'{informações[2]}.')
