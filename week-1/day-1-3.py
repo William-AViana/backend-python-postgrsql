3f592a1da2820403ddad87de5bd561154efe1521
William-AViana <willdev.viana@gmail.com>
2025-09-15 21:46:15 -0300

    update sum with two number function

--- week-1/day-1-3.py
+++ week-1/day-1-3.py
@@ -5,7 +5,10 @@ operator = input('Qual operação você quer fazer? '
                 '(3) Divisão \n (4) Multiplicação \n '
                 'Opção: ')
 
+        
 if(operator == '1'):
-    result_sum = sum(int(num) for num in numbers_user)
-print(result_sum)
+    def soma(numbers):
+        result = sum(int(num) for num in numbers)
+        print(result)
 
+    soma(numbers_user)
