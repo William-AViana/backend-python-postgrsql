fbd6f3291d4c8ba17c9637ae15dc6285126fe8f8
William-AViana <willdev.viana@gmail.com>
2025-09-16 22:10:41 -0300

    update function math

--- week-1/day-1-3.py
+++ week-1/day-1-3.py
@@ -1,14 +1,36 @@
-numbers_user = input('Digite dois números separados por (, ): ').split(', ')
+from functools import reduce
+
+numbers_user = list(input('Digite dois números separados por (, ): ').split(', '))
 
 operator = input('Qual operação você quer fazer? '
                 '(1) Soma \n (2) Subtração \n ' 
                 '(3) Divisão \n (4) Multiplicação \n '
                 'Opção: ')
 
-        
-if(operator == '1'):
-    def soma(numbers):
-        result = sum(int(num) for num in numbers)
-        print(result)
+numbers_user_int = [int(num) for num in numbers_user]
 
-    soma(numbers_user)
+def soma(numbers_user_int):
+    result = sum(int(num) for num in numbers_user_int)
+    print(result)
+
+def subtracao(numbers_user_int):
+    result = reduce(lambda acumulador, elemento_atual: acumulador - elemento_atual, numbers_user_int)
+    print(result)
+
+def divisao(numbers_user_int):
+    result = reduce(lambda acumulador, elemento_atual: acumulador / elemento_atual, numbers_user_int)
+    print(result)
+
+def multiplicacao(numbers_user_int):
+    result = reduce(lambda acumulador, elemento_atual: acumulador * elemento_atual, numbers_user_int)
+    print(result)
+
+match operator:
+    case "1":
+        soma(numbers_user_int)
+    case "2":
+        subtracao(numbers_user_int)
+    case "3":
+        divisao(numbers_user_int)
+    case "4":
+        multiplicacao(numbers_user_int)
