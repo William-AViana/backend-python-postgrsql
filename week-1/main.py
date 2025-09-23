20cca210266a992a34ae5a610b3f3a0d2c51db67
William-AViana <willdev.viana@gmail.com>
2025-09-22 22:01:11 -0300

    update main for fastapi

--- week-1/main.py
+++ week-1/main.py
@@ -1,31 +1,4 @@
 from fastapi import FastAPI
-import psycopg2
-from dotenv import load_dotenv
-import os
-
-load_dotent()
-
-try:
-    # Parâmetros de conexão
-    conn = psycopg2.connect(
-        database=os.getenv("DATABASE"),
-        user="seu_usuario",
-        password="sua_senha",
-        host="localhost",  # Ou o endereço do seu servidor
-        port="5432"        # Porta padrão do PostgreSQL
-    )
-
-    print("Conexão bem-sucedida!")
-
-    # ... (código para interagir com o banco de dados)
-
-except psycopg2.Error as e:
-    print(f"Erro ao conectar ao banco de dados: {e}")
-finally:
-    if conn:
-        conn.close() # Fechar a conexão ao final
-        print("Conexão fechada.")
-
 
 
 app = FastAPI()
