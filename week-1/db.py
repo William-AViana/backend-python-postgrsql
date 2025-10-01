e893c5de531d0af0f908468f770c144b4ca4cc35
William-AViana <willdev.viana@gmail.com>
2025-09-30 22:06:56 -0300

    update db file

--- week-1/db.py
+++ week-1/db.py
@@ -2,27 +2,36 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 
+load_dotenv()
 
 try:
 
-    load_dotenv()
-
-    conn = psycopg2.connect(
-        database=os.getenv("DATABASE"),
-        user=os.getenv("USER_DB"),
-        password=os.getenv("PASSWORD_DB"),
-        host=os.getenv("HOST_DB"),
-        port=os.getenv("PORT_DB")
-    )
-
-    print("Conexão bem-sucedida!")
+    def search_posts():
+        conn = psycopg2.connect(
+            database=os.getenv("DATABASE"),
+            user=os.getenv("USER_DB"),
+            password=os.getenv("PASSWORD_DB"),
+            host=os.getenv("HOST_DB"),
+            port=os.getenv("PORT_DB")
+        )
 
     # ... (código para interagir com o banco de dados)
-
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM posts")
+        posts = cur.fetchall()
+        
+        columns = [desc[0] for desc in cur.description]
+        posts = [dict(zip(columns, post))for post in posts]
+        
+        cur.close()
+        
+        return posts
+    
 except psycopg2.Error as e:
     print(f"Erro ao conectar ao banco de dados: {e}")
 finally:
-   if conn:
-        conn.close() # Fechar a conexão ao final
-        print("Conexão fechada.")
+    
+    print("Conexão fechada.")
 
+if __name__ == "__main__":
+    search_posts()
